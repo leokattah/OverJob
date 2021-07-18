b84e4b496632b959120353fea48a131cc2e3f06a
leokattah <leokattah@gmail.com>
2021-07-18 08:31:55 -0300

    day 5 started

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
 }
+// next.config.js
+// const withSass = require('@zeit/next-sass')
+// module.exports = withSass({
+// cssModules: true
+// })
+
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS()
