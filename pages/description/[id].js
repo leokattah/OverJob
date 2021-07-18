454dfeaeaaa1952fd75e13ed60e9feb56feb0e8d
leokattah <leokattah@gmail.com>
2021-07-18 09:32:05 -0300

    day 5 done

--- pages/description/[id].js
+++ pages/description/[id].js
@@ -1,18 +1,42 @@
-export default function description ({job}){
- return <h1>description</h1>
+import styles from ' ../../../styles/Description.module.css ';
+import Header from '../../src/components/Header';
+import CardDetails from '../../src/components/Card'
+import axios from 'axios'
+
+export default function description({ job }) {
+  return (
+    <div className={style.container}>
+      <Header showSearch={false} />
+      <div className={styles.card}>
+        {
+        job &&
+         <CardDetails
+         title={job.title}
+         description={job.description}
+         enterprise={job.enterprise}
+         day={job.day}
+         local={` ${job.city} - ${job.state} `}
+         />
+        }
+        </div>
+    </div>
+  );
 }
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
   return {
-    path: [ ],
+    paths: [],
     fallBack: true,
-  }
+  };
 }
 
-export async function getStaticProps({ params: {id} }){
-
-const {data} = await axios.get('http://localhost:3000/api/jobs?secret=over');
-return {
-  props: {
-    job: data.jobs
-  }}}
+export async function getStaticProps({ params: { id } }) {
+  const { data } = await axios.get(
+    'http://localhost:3000/api/jobs?{id}&secret=over'
+  );
+  return {
+    props: {
+      job: data.jobs,
+    },
+  };
+}
